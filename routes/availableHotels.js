1e78b41455b14e77349893c0ccfc656aeafde762
mohammad-okasha <mhmmadokasha@gmail.com>
2024-05-31 11:14:10 +0300

    filter rooms ok

--- routes/availableHotels.js
+++ routes/availableHotels.js
@@ -7,12 +7,32 @@ const hotelModel = (await import('../models/Hotels.js')).default;
 const roomModel = (await import('../models/Rooms.js')).default;
 
 router.post('/', async (request, response) => {
-    const {hotelId,range,roomType} = request.body
+    const { hotelId, searchTime, roomType } = request.body;
 
-    const data = await roomModel.find({hotelId:hotelId});
+    // Build the query object based on the presence of hotelId and roomType
+    const query = {};
+    if (hotelId) query.hotelId = hotelId;
+    if (roomType) query.roomType = roomType;
 
-    console.log(hotelId)
-    response.json(data);
+    try {
+        const data = await roomModel.find(query);
+
+        // If searchTime is provided, filter the dates in the range
+        if (searchTime) {
+            const [searchYear, searchMonth] = searchTime.split('-').map(Number);
+
+            data.forEach(room => {
+                room.range = room.range.filter(dateObj => {
+                    const date = new Date(dateObj);
+                    return date.getUTCFullYear() === searchYear && date.getUTCMonth() + 1 === searchMonth;
+                });
+            });
+        }
+
+        response.json(data);
+    } catch (error) {
+        response.status(500).json({ error: 'An error occurred while fetching the data.' });
+    }
 });
 
 
