2672012d1ba90b0664621acecba79be608157641
mohmmad-okasha <mhmmadokasha@gmail.com>
2024-06-19 00:01:55 +0300

    booking filter

--- routes/bookings.js
+++ routes/bookings.js
@@ -5,14 +5,208 @@ const router = express.Router();
 //import bookings model file
 const bookingModel = (await import('../models/Bookings.js')).default;
 
-router.get('/', async (request, response) => {
-    let data = await bookingModel.find();
+router.post('/', async (request, response) => {
+    const { filter, range } = request.body;
+    console.log('filter :' + filter)
+
+    let data = {}
+    if (filter) {
+        switch (filter) {
+            case 'Custom Date': {
+                if (range[0]) {
+                    const now = dayjs();
+
+                    const start = dayjs(range[0]).toDate();
+                    const end = dayjs(range[1]).add(1, 'day').toDate();
+
+                    data = await bookingModel.find({
+                        bookDate: {
+                            $gte: start,
+                            $lte: end
+                        }
+                    });
+                }else{//if clear range load all
+                    data = await bookingModel.find();
+                }
+                break;
+            }
+            case 'Daily': { //last week
+                // Get the current date
+                const now = dayjs();
+
+                // Calculate the start and end of today
+                const startOfToday = now.startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                // Find all bookings for today
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfToday,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+            case 'Weekly': {
+                const now = dayjs();
+
+                const startOfLastWeek = now.subtract(1, 'week').startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfLastWeek,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+            case 'Monthly': {
+                const now = dayjs();
+
+                const startOfLastMonth = now.subtract(1, 'month').startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfLastMonth,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+            case 'Yearly': {
+                const now = dayjs();
+
+                const startOfLastYear = now.subtract(1, 'year').startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfLastYear,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+            default :data = await bookingModel.find();
+        }
+    }
+    else {
+        data = await bookingModel.find();
+    }
+
     // Format the dates for each booking
     data = data.map(booking => ({
         ...booking._doc, // Use _doc to access the document data if using Mongoose
         bookDate: dayjs(booking.bookDate).format('YYYY-MM-DD HH:mm'),
-        formatedRange: (dayjs(booking.dates[0]).format('YYYY-MM-DD')) + ' - '+dayjs(booking.dates[1]).format('YYYY-MM-DD')
-    })); 
+        formatedRange: (dayjs(booking.dates[0]).format('YYYY-MM-DD')) + ' - ' + dayjs(booking.dates[1]).format('YYYY-MM-DD')
+    }));
+    response.json(data);
+});
+
+router.get('/', async (request, response) => {
+
+    const { filter, range2 } = request.query;
+    const range = request.query.range ? request.query.range.split(',') : [];
+    console.log('range ' + range)
+    let data = {}
+    if (filter) {
+        switch (filter) {
+            case 'daily': { //last week
+                // Get the current date
+                const now = dayjs();
+
+                // Calculate the start and end of today
+                const startOfToday = now.startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                // Find all bookings for today
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfToday,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+            case 'weekly': {
+                const now = dayjs();
+
+                const startOfLastWeek = now.subtract(1, 'week').startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfLastWeek,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+            case 'monthly': {
+                const now = dayjs();
+
+                const startOfLastMonth = now.subtract(1, 'month').startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfLastMonth,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+            case 'yearly': {
+                const now = dayjs();
+
+                const startOfLastYear = now.subtract(1, 'year').startOf('day').toDate();
+                const endOfToday = now.endOf('day').toDate();
+
+                data = await bookingModel.find({
+                    bookDate: {
+                        $gte: startOfLastYear,
+                        $lte: endOfToday
+                    }
+                });
+
+                break;
+            }
+        }
+    }
+    else if (range.length) {
+        console.log('range ' + range)
+
+        const now = dayjs();
+
+        const start = range[0];
+        const end = range[1];
+
+        data = await bookingModel.find({
+            bookDate: {
+                $gte: start,
+                $lte: end
+            }
+        });
+    }
+    else {
+        data = await bookingModel.find();
+    }
+
+    // Format the dates for each booking
+    data = data.map(booking => ({
+        ...booking._doc, // Use _doc to access the document data if using Mongoose
+        bookDate: dayjs(booking.bookDate).format('YYYY-MM-DD HH:mm'),
+        formatedRange: (dayjs(booking.dates[0]).format('YYYY-MM-DD')) + ' - ' + dayjs(booking.dates[1]).format('YYYY-MM-DD')
+    }));
     response.json(data);
 });
 
